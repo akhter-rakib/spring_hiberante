bb23c195f672ea41fbad15cfd10b0428e9c10f01
akter_rakib <rakibccj@gmail.com>
2020-05-14 01:38:20 +0600

    Bean lifecycle by Annotation

--- src/com/rakib/annotaion/CricketCoach.java
+++ src/com/rakib/annotaion/CricketCoach.java
@@ -1,5 +1,8 @@
 package com.rakib.annotaion;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -13,4 +16,17 @@ public class CricketCoach implements Coach {
 		return "Batting practice regular 30 hour";
 	}
 
+	public CricketCoach() {
+		System.out.println("Inside Constructor");
+	}
+
+	@PostConstruct
+	public void initMethod() {
+		System.out.println("Post Constructor");
+	}
+
+	@PreDestroy
+	public void destoryMethod() {
+		System.out.println("Before bean destry ");
+	}
 }
